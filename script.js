2b1206ae97873121f0068d3d6cd5844faaffde8f
Slice Wess <ernestwesson86@gmail.com>
2020-09-23 23:30:26 -0400

    working on question functionality

--- script.js
+++ script.js
@@ -1,47 +1,85 @@
-let myQuestions = [
-	question1 = {
-		question: "What is a Javascript?",
-		correctAnsw: "Programming Language",
-		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
-	},
-	question2 = {
-		question: "What is a Javascript?",
-		correctAnsw: "Programming Language",
-		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
-	},
-	question3 = {
-		question: "What is a Javascript?",
-		correctAnsw: "Programming Language",
-		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
-	},
-	question4 = {
-		question: "What is a Javascript?",
-		correctAnsw: "Programming Language",
-		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
+let startButton = document.getElementById('startButton')
+
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz()  {
+	console.log('Started')
+	setQuestion()
+
+}
+
+function nextQuestion() {
+
+
+
+}
+
+function selectAnswer() {
+
+
+}
+
+let questions = [
+
+	{
+
+		
 	}
-	
+
+
+
+
 ]
 
-// empty array for highscores
-
-//Header
-let viewScores = document.getElementById("viewScores");
-let timer = document.getElementById("timer");
-
-//Main section
-let startBtn = document.getElementById("start-game");
-
-//Questions & Answers
-
-//Highscore Page
-
-
-//Add event hadnler to the elements
-
-startBtn.addEventListener("click", startQuiz);
 
 
 
-function startQuiz() {
-	alert("quiz has started");
-}
+
+// let myQuestions = [
+// 	question1 = {
+// 		question: "What is a Javascript?",
+// 		correctAnsw: "Programming Language",
+// 		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
+// 	},
+// 	question2 = {
+// 		question: "What is a Javascript?",
+// 		correctAnsw: "Programming Language",
+// 		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
+// 	},
+// 	question3 = {
+// 		question: "What is a Javascript?",
+// 		correctAnsw: "Programming Language",
+// 		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
+// 	},
+// 	question4 = {
+// 		question: "What is a Javascript?",
+// 		correctAnsw: "Programming Language",
+// 		answers: ["Library", "Programming Language", "Backend Language", "UI Framework"]
+// 	}
+	
+
+
+// // empty array for highscores
+
+// //Header
+// let viewScores = document.getElementById("viewScores");
+// let timer = document.getElementById("timer");
+
+// //Main section
+// let startBtn = document.getElementById("start-game");
+
+// //Questions & Answers
+
+// //Highscore PageP
+
+
+// //Add event hadnler to the elements
+
+// startBtn.addEventListener("click", startQuiz);
+
+
+
+// function startQuiz() {
+// 	alert("quiz has started");
+// }
